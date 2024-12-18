b8a090094d2d4ca6f545c002a81f3ac848123c30
MateuszKopka <mateusz.kopka@vp.pl>
2024-12-18 21:50:19 +0100

    Initial commit

---
+++ src/main/java/com/example/demo/UserController.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class UserController {
+
+    @GetMapping("/user")
+    public String getUser() {
+        return "Witaj! To jest podstawowa aplikacja dla IntelliJ IDEA 2024";
+    }
+}
